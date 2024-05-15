ab83d76a0d12a448d501192440f8d5d36e479f96
Sayantan Majumder <quic_smajumde@quicinc.com>
2024-05-15 14:10:20 +0530

    mmrm-driver: Update hardcoded paths and disable mmrm for LW
    Replace  hardcoded path and disablement of mmrm driver for LW
    
    Signed-off-by: Sayantan Majumder <quic_smajumde@quicinc.com>
    Change-Id: Ia4e5b568a50d4fc3110acfd6606ffc12241de568

--- Android.mk
+++ Android.mk
@@ -1,4 +1,8 @@
-MMRM_BLD_DIR := $(shell pwd)/vendor/qcom/opensource/mmrm-driver
+ifneq ($(TARGET_BOARD_PLATFORM), monaco)
+BOARD_OPENSOURCE_DIR ?= vendor/qcom/opensource
+BOARD_COMMON_DIR ?= device/qcom/common
+
+MMRM_BLD_DIR := $(shell pwd)/$(BOARD_OPENSOURCE_DIR)/mmrm-driver
 
 # Build msm-mmrm.ko
 ###########################################################
@@ -7,7 +11,7 @@ KBUILD_OPTIONS := MMRM_ROOT=$(MMRM_BLD_DIR)
 KBUILD_OPTIONS += BOARD_PLATFORM=$(TARGET_BOARD_PLATFORM)
 ###########################################################
 
-DLKM_DIR   := device/qcom/common/dlkm
+DLKM_DIR   := $(BOARD_COMMON_DIR)/dlkm
 
 LOCAL_PATH := $(call my-dir)
 LOCAL_MODULE_DDK_BUILD    := true
@@ -47,3 +51,4 @@ LOCAL_MODULE_PATH         := $(KERNEL_MODULES_OUT)
 # Include kp_module.ko in the /vendor/lib/modules (vendor.img)
 # BOARD_VENDOR_KERNEL_MODULES += $(LOCAL_MODULE_PATH)/$(LOCAL_MODULE)
 include $(DLKM_DIR)/Build_external_kernelmodule.mk
+endif

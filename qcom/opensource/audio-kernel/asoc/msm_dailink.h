f8e5520c30a1c04a66fc93526f393bee23ff4962
Fiqri Ardyansyah <fiqri191002@gmail.com>
2024-09-09 17:50:12 +0700

    audio-kernel: Import xiaomi changes from marble-s-oss
    * Drop support for other devices, and hardcode for marble.
    
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- qcom/opensource/audio-kernel/asoc/msm_dailink.h
+++ qcom/opensource/audio-kernel/asoc/msm_dailink.h
@@ -314,12 +314,14 @@ SND_SOC_DAILINK_DEFS(sec_tdm_tx_0,
 
 SND_SOC_DAILINK_DEFS(tert_tdm_rx_0,
 	DAILINK_COMP_ARRAY(COMP_CPU("snd-soc-dummy-dai")),
-	DAILINK_COMP_ARRAY(COMP_CODEC("msm-stub-codec.1", "msm-stub-rx")),
+	DAILINK_COMP_ARRAY(COMP_CODEC("aw882xx_smartpa.2-0035", "aw882xx-aif-2-35"),
+			   COMP_CODEC("aw882xx_smartpa.2-0034", "aw882xx-aif-2-34")),
 	DAILINK_COMP_ARRAY(COMP_PLATFORM("snd-soc-dummy")));
 
 SND_SOC_DAILINK_DEFS(tert_tdm_tx_0,
 	DAILINK_COMP_ARRAY(COMP_CPU("snd-soc-dummy-dai")),
-	DAILINK_COMP_ARRAY(COMP_CODEC("msm-stub-codec.1", "msm-stub-tx")),
+	DAILINK_COMP_ARRAY(COMP_CODEC("aw882xx_smartpa.2-0035", "aw882xx-aif-2-35"),
+			   COMP_CODEC("aw882xx_smartpa.2-0034", "aw882xx-aif-2-34")),
 	DAILINK_COMP_ARRAY(COMP_PLATFORM("snd-soc-dummy")));
 
 SND_SOC_DAILINK_DEFS(quat_tdm_rx_0,

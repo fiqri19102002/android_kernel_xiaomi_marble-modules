1c76c73a3ea979d704789d3f7fa78ead60e5c1a2
Sayantan Majumder <quic_smajumde@quicinc.com>
2024-09-30 15:54:58 +0530

    mmrm: Disable mmrm for vienna
    Disabled mmrm for vienna target
    
    Change-Id: Ie31e463149ec451218f2e7e1216970bece0c4bdf
    Signed-off-by: Sayantan Majumder <quic_smajumde@quicinc.com>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,4 @@
-ifneq ($(TARGET_BOARD_PLATFORM), monaco)
+ifneq ($(filter $(TARGET_BOARD_PLATFORM), monaco vienna),$(TARGET_BOARD_PLATFORM))
 BOARD_OPENSOURCE_DIR ?= vendor/qcom/opensource
 BOARD_COMMON_DIR ?= device/qcom/common
 
--- mmrm_kernel_board.mk
+++ mmrm_kernel_board.mk
@@ -1,4 +1,4 @@
-ifneq ($(TARGET_BOARD_PLATFORM), monaco)
+ifneq ($(filter $(TARGET_BOARD_PLATFORM), monaco vienna),$(TARGET_BOARD_PLATFORM))
 ifeq ($(call is-board-platform-in-list,$(TARGET_BOARD_PLATFORM)),true)
 BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/msm-mmrm.ko
 BOARD_VENDOR_RAMDISK_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/msm-mmrm.ko

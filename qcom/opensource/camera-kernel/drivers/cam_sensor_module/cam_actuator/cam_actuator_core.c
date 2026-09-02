aa1693cda5a7a57e2e2067b0917587e4c2846edb
Pzqqt <pzqqt88198@gmail.com>
2026-09-02 11:12:29 +0800

    camera-kernel: Fix a large number of memleak issues
    Kernel Memory Leak Detector detected two memory leaks:
    
    unreferenced object 0xffffff87f9bc4080 (size 128):
      comm "vendor.qti.came", pid 1459, jiffies 4294880500 (age 1883.344s)
      hex dump (first 32 bytes):
        08 00 00 01 68 00 00 00 00 00 00 00 00 00 00 00  ....h...........
        00 00 00 00 00 00 00 00 00 00 00 00 02 00 00 00  ................
      backtrace:
        [<00000000b0bda9d5>] slab_post_alloc_hook+0xa0/0x3e0
        [<000000001babca28>] __kmalloc+0x26c/0x480
        [<00000000d35de005>] kvmalloc_node+0x8c/0x220
        [<0000000023c69348>] cam_common_mem_kdup+0x60/0x1a0 [camera]
        [<000000009b1ff553>] cam_packet_util_copy_pkt_to_kmd+0x44/0x120 [camera]
        [<000000003e202f1e>] cam_handle_mem_ptr+0x60/0x4a0 [camera]
        [<00000000d9010815>] cam_sensor_driver_cmd+0x500/0x1900 [camera]
        [<00000000917e7e2e>] cam_sensor_subdev_ioctl+0x38/0x190 [camera]
        [<00000000ad303e69>] subdev_do_ioctl_lock+0xb74/0xcb0
        [<00000000fa433d17>] video_usercopy+0x36c/0xaa0
        [<00000000326b2e28>] subdev_ioctl+0x18/0x30
        [<00000000cfb3672c>] v4l2_ioctl+0x74/0xe0
        [<00000000b4240607>] __arm64_sys_ioctl+0x118/0x190
        [<0000000036aebbb7>] el0_svc_common.llvm.16691463139245361223+0x118/0x220
        [<00000000bc8c068a>] el0_svc+0x24/0xa0
        [<00000000810ad938>] el0_sync_handler+0xf8/0x120
    unreferenced object 0xffffff89e21af800 (size 512):
      comm "HwBinder:1459_1", pid 14065, jiffies 4295155433 (age 967.024s)
      hex dump (first 32 bytes):
        02 00 00 00 28 01 00 00 00 00 00 00 00 00 00 00  ....(...........
        00 00 00 00 00 00 00 00 00 00 00 00 01 00 00 00  ................
      backtrace:
        [<00000000b0bda9d5>] slab_post_alloc_hook+0xa0/0x3e0
        [<000000001babca28>] __kmalloc+0x26c/0x480
        [<00000000d35de005>] kvmalloc_node+0x8c/0x220
        [<0000000023c69348>] cam_common_mem_kdup+0x60/0x1a0 [camera]
        [<000000009b1ff553>] cam_packet_util_copy_pkt_to_kmd+0x44/0x120 [camera]
        [<000000004fe00716>] cam_sensor_driver_cmd+0x754/0x1900 [camera]
        [<00000000917e7e2e>] cam_sensor_subdev_ioctl+0x38/0x190 [camera]
        [<00000000ad303e69>] subdev_do_ioctl_lock+0xb74/0xcb0
        [<00000000fa433d17>] video_usercopy+0x36c/0xaa0
        [<00000000326b2e28>] subdev_ioctl+0x18/0x30
        [<00000000cfb3672c>] v4l2_ioctl+0x74/0xe0
        [<00000000b4240607>] __arm64_sys_ioctl+0x118/0x190
        [<0000000036aebbb7>] el0_svc_common.llvm.16691463139245361223+0x118/0x220
        [<00000000bc8c068a>] el0_svc+0x24/0xa0
        [<00000000810ad938>] el0_sync_handler+0xf8/0x120
        [<00000000f20aee2c>] el0_sync+0x190/0x1c0
    ...
    
    Fix it, and incidentally fix any other potential memory leaks that have been discovered.
    
    Change-Id: Ife73f19f77fc664eddc6aca82ccf9ee060bfe1a6
    Signed-off-by: Pzqqt <pzqqt88198@gmail.com>
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- qcom/opensource/camera-kernel/drivers/cam_sensor_module/cam_actuator/cam_actuator_core.c
+++ qcom/opensource/camera-kernel/drivers/cam_sensor_module/cam_actuator/cam_actuator_core.c
@@ -669,7 +669,8 @@ int32_t cam_actuator_i2c_pkt_parse(struct cam_actuator_ctrl_t *a_ctrl,
 		if (!csl_packet->num_cmd_buf) {
 			CAM_ERR(CAM_ACTUATOR, "Invalid num_cmd_buffer = %d",
 				csl_packet->num_cmd_buf);
-			return -EINVAL;
+			rc = -EINVAL;
+			goto end;
 		}
 		if (a_ctrl->cam_act_state < CAM_ACTUATOR_CONFIG) {
 			rc = -EINVAL;
@@ -784,9 +785,6 @@ int32_t cam_actuator_i2c_pkt_parse(struct cam_actuator_ctrl_t *a_ctrl,
 		rc = -EINVAL;
 		goto end;
 	}
-
-	cam_mem_put_cpu_buf(config.packet_handle);
-	return rc;
 
 end:
 	cam_common_mem_free(csl_packet);
--- qcom/opensource/camera-kernel/drivers/cam_sensor_module/cam_sensor/cam_sensor_core.c
+++ qcom/opensource/camera-kernel/drivers/cam_sensor_module/cam_sensor/cam_sensor_core.c
@@ -383,7 +383,7 @@ static int32_t cam_sensor_i2c_pkt_parse(struct cam_sensor_ctrl_t *s_ctrl,
 	rc = cam_packet_util_validate_cmd_desc(cmd_desc);
 	if (rc) {
 		CAM_ERR(CAM_SENSOR, "Invalid cmd desc ret: %d", rc);
-		return rc;
+		goto end;
 	}
 
 	rc = cam_sensor_i2c_command_parser(&s_ctrl->io_master_info,
@@ -410,9 +410,6 @@ static int32_t cam_sensor_i2c_pkt_parse(struct cam_sensor_ctrl_t *s_ctrl,
 		i2c_reg_settings->request_id =
 			csl_packet->header.request_id;
 	}
-
-	cam_mem_put_cpu_buf(config.packet_handle);
-	return rc;
 
 end:
 	cam_common_mem_free(csl_packet);
@@ -713,7 +710,7 @@ int32_t cam_handle_mem_ptr(uint64_t handle, uint32_t cmd,
 	rc = cam_packet_util_validate_cmd_desc(cmd_desc);
 	if (rc) {
 		CAM_ERR(CAM_SENSOR, "Invalid cmd desc ret: %d", rc);
-		return rc;
+		goto end;
 	}
 
 	probe_ver = pkt->header.op_code & 0xFFFFFF;
@@ -722,7 +719,7 @@ int32_t cam_handle_mem_ptr(uint64_t handle, uint32_t cmd,
 	for (i = 0; i < pkt->num_cmd_buf; i++) {
 		rc = cam_packet_util_validate_cmd_desc(&cmd_desc[i]);
 		if (rc)
-			return rc;
+			goto end;
 
 		if (!(cmd_desc[i].length))
 			continue;
@@ -763,9 +760,6 @@ int32_t cam_handle_mem_ptr(uint64_t handle, uint32_t cmd,
 		}
 		cam_mem_put_cpu_buf(cmd_desc[i].mem_handle);
 	}
-
-	cam_mem_put_cpu_buf(handle);
-	return rc;
 
 end:
 	cam_common_mem_free(pkt);
